a4006730597d871ce6432f582a5afc8d91ff7f1e
SystemGDS <systemgds@gmail.com>
2023-05-08 11:12:47 -0500

    Deploy

--- api/src/db/config/config.ts
+++ api/src/db/config/config.ts
@@ -15,7 +15,7 @@ export = {
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": String(process.env.DB_USERNAME),
