19a71d75e5fa0eb50c13bec37e7440af6be39e3d
SystemGDS <systemgds@gmail.com>
2023-05-08 07:02:46 -0500

    Deploy Actualizado

--- api/src/server.ts
+++ api/src/server.ts
@@ -13,6 +13,14 @@ const app = express();
     app.use(cors());
     
     app.use("/",router);
+    app.use((_req, res, next) => {
+      // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+      res.header('Access-Control-Allow-Origin', '*'); 
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+      next();
+    });
     
     app.listen(PORT, ()=>{
       console.log(`Server listening on ${PORT}`)
